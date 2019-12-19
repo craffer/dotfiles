2a4b6e60eea6b57256865417ed28bc28604b0edf
Conor Rafferty <craffer@umich.edu>
2019-12-19 10:52:48 -0500

    Newline EOF

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -23,3 +23,4 @@ alias googler="googler -n 3"
 alias createfs="~/UofM/F19/EECS_482/p4/utility/createfs_macos"
 alias showfs="~/UofM/F19/EECS_482/p4/utility/showfs_macos"
 alias say="say --interactive=green"
+
