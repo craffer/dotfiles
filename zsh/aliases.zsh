f99c4215c0286cc565692376c44c9a48d21366a2
Conor Rafferty <craffer@umich.edu>
2019-12-25 19:25:09 -0500

    Rearrange aliases file

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,26 +1,34 @@
 # Set personal aliases, overriding those provided by oh-my-zsh libs,
 # plugins, and themes.
 # For a full list of active aliases, run `alias`.
-alias publicip="curl https://ipinfo.io/ip"
-alias activate="source env/bin/activate"
+
+# core
 alias ls="colorls"
-alias weather="curl http://wttr.in/ann_arbor\?Tn1"
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
-alias smell="say --interactive=green -v Albert smell"
+alias mv="mv -iv"
+alias cp="cp -iv"
+alias v="vim"
+
+# macos
+alias ql="quick-look"
+alias preview="quick-look"
+alias notion="open /Applications/Notion.app"
+alias chrome="open -a 'Google Chrome'"
+
+# schoolwork
 alias kill_vm="VBoxManage controlvm 388-kali poweroff"
 alias vm_ssh="ssh -p 2222 conor@127.0.0.1"
 alias 485tree="tree --matchdirs -I 'env|__pycache__|node_modules|*.egg*'"
-alias notion="open /Applications/Notion.app"
-alias preview="quick-look"
-alias ql="quick-look"
-alias mv="mv -iv"
-alias cp="cp -iv"
-alias vimless="/usr/share/vim/vim80/macros/less.sh"
-alias googler="googler -n 3"
 alias createfs="~/UofM/F19/EECS_482/p4/utility/createfs_macos"
 alias showfs="~/UofM/F19/EECS_482/p4/utility/showfs_macos"
-alias say="say --interactive=green"
-alias chrome="open -a 'Google Chrome'"
 
+# other
+alias weather="curl http://wttr.in/ann_arbor\?Tn1"
+alias smell="say --interactive=green -v Albert smell"
+alias publicip="curl https://ipinfo.io/ip"
+alias activate="source env/bin/activate"
+alias vimless="/usr/share/vim/vim80/macros/less.sh"
+alias googler="googler -n 3"
+alias say="say --interactive=green"
