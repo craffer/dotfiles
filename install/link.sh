843a3bb521d8d12331638e6e8a4e60c83ca62ded
Conor Rafferty <craffer@umich.edu>
2020-01-06 14:08:32 -0500

    Remove typos

--- install/link.sh
+++ install/link.sh
@@ -5,18 +5,18 @@ ln -svFh ~/.dotfiles/bash/.bash_profile ~
 ln -svFh ~/.dotfiles/bash/.bashrc ~
 
 # config
-ln -svFhFh ~/.dotfiles/config ~/.config
+ln -svFh ~/.dotfiles/config ~/.config
 
 # git
 ln -svFh ~/.dotfiles/git/.gitconfig ~
 
 # vim
-ln -svFhFh ~/.dotfiles/vim ~/.vim
+ln -svFh ~/.dotfiles/vim ~/.vim
 ln -svFh ~/.dotfiles/vim/vimrc ~/.vimrc
 
 # zsh
-ln -svFhFh ~/.dotfiles/zsh/.p10k.zsh ~
-ln -svFhFh ~/.dotfiles/zsh/.zshrc ~
+ln -svFh ~/.dotfiles/zsh/.p10k.zsh ~
+ln -svFh ~/.dotfiles/zsh/.zshrc ~
 ln -svFh ~/.dotfiles/zsh/aliases.zsh ~/.oh-my-zsh/custom
 ln -svFh ~/.dotfiles/zsh/functions.zsh ~/.oh-my-zsh/custom
 ln -svFh ~/.dotfiles/zsh/shortcuts.zsh ~/.oh-my-zsh/custom
