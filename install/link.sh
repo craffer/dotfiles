f793579fdda34d1aef514f20b1da5640221d335c
Conor Rafferty <craffer@umich.edu>
2020-01-06 14:10:27 -0500

    Remove macOS specific -h command

--- install/link.sh
+++ install/link.sh
@@ -1,28 +1,28 @@
 # link the files in here to the root directory
 
 # bash
-ln -svFh ~/.dotfiles/bash/.bash_profile ~
-ln -svFh ~/.dotfiles/bash/.bashrc ~
+ln -svF ~/.dotfiles/bash/.bash_profile ~
+ln -svF ~/.dotfiles/bash/.bashrc ~
 
 # config
-ln -svFhFh ~/.dotfiles/config ~/.config
+ln -svF ~/.dotfiles/config ~/.config
 
 # git
-ln -svFh ~/.dotfiles/git/.gitconfig ~
+ln -svF ~/.dotfiles/git/.gitconfig ~
 
 # vim
-ln -svFhFh ~/.dotfiles/vim ~/.vim
-ln -svFh ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -svF ~/.dotfiles/vim ~/.vim
+ln -svF ~/.dotfiles/vim/vimrc ~/.vimrc
 
 # zsh
-ln -svFhFh ~/.dotfiles/zsh/.p10k.zsh ~
-ln -svFhFh ~/.dotfiles/zsh/.zshrc ~
-ln -svFh ~/.dotfiles/zsh/aliases.zsh ~/.oh-my-zsh/custom
-ln -svFh ~/.dotfiles/zsh/functions.zsh ~/.oh-my-zsh/custom
-ln -svFh ~/.dotfiles/zsh/shortcuts.zsh ~/.oh-my-zsh/custom
+ln -svF ~/.dotfiles/zsh/.p10k.zsh ~
+ln -svF ~/.dotfiles/zsh/.zshrc ~
+ln -svF ~/.dotfiles/zsh/aliases.zsh ~/.oh-my-zsh/custom
+ln -svF ~/.dotfiles/zsh/functions.zsh ~/.oh-my-zsh/custom
+ln -svF ~/.dotfiles/zsh/shortcuts.zsh ~/.oh-my-zsh/custom
 
 # other
-ln -svFh ~/.dotfiles/other/.eslintrc.js ~
-ln -svFh ~/.dotfiles/other/.always_forget.txt ~
-ln -svFh ~/.dotfiles/other/settings.json ~/Library/Application\ Support/Code/User
-ln -svFh ~/.dotfiles/other/itermprofiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles
+ln -svF ~/.dotfiles/other/.eslintrc.js ~
+ln -svF ~/.dotfiles/other/.always_forget.txt ~
+ln -svF ~/.dotfiles/other/settings.json ~/Library/Application\ Support/Code/User
+ln -svF ~/.dotfiles/other/itermprofiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles
