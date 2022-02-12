3ee7a20b31dcc1e5ed2f9d08c2fcaac9dbfbc495
Conor Rafferty <craffer@umich.edu>
2022-02-12 14:27:51 -0800

    Change dotfiles repo location

--- install/link.sh
+++ install/link.sh
@@ -1,28 +1,28 @@
 # link the files in here to the root directory
 
 # bash
-ln -svFh ~/.dotfiles/bash/.bash_profile ~
-ln -svFh ~/.dotfiles/bash/.bashrc ~
+ln -svFh ~/dev/projects/dotfiles/bash/.bash_profile ~
+ln -svFh ~/dev/projects/dotfiles/bash/.bashrc ~
 
 # config
-ln -svFh ~/.dotfiles/config ~/.config
+ln -svFh ~/dev/projects/dotfiles/config ~/.config
 
 # git
-ln -svFh ~/.dotfiles/git/.gitconfig ~
+ln -svFh ~/dev/projects/dotfiles/git/.gitconfig ~
 
 # vim
-ln -svFh ~/.dotfiles/vim ~/.vim
-ln -svFh ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -svFh ~/dev/projects/dotfiles/vim ~/.vim
+ln -svFh ~/dev/projects/dotfiles/vim/vimrc ~/.vimrc
 
 # zsh
-ln -svFh ~/.dotfiles/zsh/.p10k.zsh ~
-ln -svFh ~/.dotfiles/zsh/.zshrc ~
-ln -svFh ~/.dotfiles/zsh/aliases.zsh ~/.zsh/aliases.zsh
-ln -svFh ~/.dotfiles/zsh/functions.zsh ~/.zsh/functions.zsh
-ln -svFh ~/.dotfiles/zsh/shortcuts.zsh ~/.shortcuts.zsh
+ln -svFh ~/dev/projects/dotfiles/zsh/.p10k.zsh ~
+ln -svFh ~/dev/projects/dotfiles/zsh/.zshrc ~
+ln -svFh ~/dev/projects/dotfiles/zsh/aliases.zsh ~/.zsh/aliases.zsh
+ln -svFh ~/dev/projects/dotfiles/zsh/functions.zsh ~/.zsh/functions.zsh
+ln -svFh ~/dev/projects/dotfiles/zsh/shortcuts.zsh ~/.shortcuts.zsh
 
 # other
-ln -svFh ~/.dotfiles/other/.eslintrc.js ~
-ln -svFh ~/.dotfiles/other/.always_forget.txt ~
-ln -svFh ~/.dotfiles/other/settings.json ~/Library/Application\ Support/Code/User
-ln -svFh ~/.dotfiles/other/itermprofiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles
+ln -svFh ~/dev/projects/dotfiles/other/.eslintrc.js ~
+ln -svFh ~/dev/projects/dotfiles/other/.always_forget.txt ~
+ln -svFh ~/dev/projects/dotfiles/other/settings.json ~/Library/Application\ Support/Code/User
+ln -svFh ~/dev/projects/dotfiles/other/itermprofiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles
