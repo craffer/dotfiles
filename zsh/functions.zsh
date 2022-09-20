bcd00ad052f1f5f20ab3dfb19eeeb76d528c89d2
Conor Rafferty <conor.rafferty@salesforce.com>
2022-09-20 13:53:54 -0700

    Add vault-login function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -53,3 +53,27 @@ activate_blt()
 {
     source ~/blt/env.sh
 }
+
+vault-login()
+{
+    # Export the AWS credentials for one of the roles with the appropriate read/write privilege before running the commands.
+    # Or, if you are using vaultconf-basic* pipelines, use *PCSKDeveloperRole*
+    if [ $# -eq 2 ]
+    then
+        export VAULT_ROLE=$2
+    else
+        export VAULT_ROLE=kv_loganalytics-rw
+    fi
+    export FI=$1
+    export AWS_REGION=us-east-1 # STS service is always in us-east-1
+    if [ $FI = "aws-giadev1-usgoveast1" ]
+    then
+        echo "In GIA2H Dev"
+        export AWS_REGION=us-gov-east-1
+        export AWS_STS_REGIONAL_ENDPOINTS=regional
+    fi
+    export AWS_LOGIN_HEADER=api.vault.secrets.$FI.aws.sfdc.cl
+    export VAULT_SKIP_VERIFY=1
+    export VAULT_ADDR=https://$AWS_LOGIN_HEADER
+    export VAULT_TOKEN=$(vault login --token-only --method=aws header_value=$AWS_LOGIN_HEADER role=$VAULT_ROLE region=$AWS_REGION)
+}
