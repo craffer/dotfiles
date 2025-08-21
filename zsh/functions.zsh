eeeb0bd0914c2bd77743f3dfc7c48269814ade61
Conor Rafferty <conor.rafferty@salesforce.com>
2025-08-21 11:38:56 -0700

    Setup automatic k8s contexts

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -77,3 +77,46 @@ vault-login()
     export VAULT_ADDR=https://$AWS_LOGIN_HEADER
     export VAULT_TOKEN=$(vault login --token-only --method=aws header_value=$AWS_LOGIN_HEADER role=$VAULT_ROLE region=$AWS_REGION)
 }
+
+# k8s contexts
+_k8s_context_setup() {
+    local env=$1
+    local aws_context="moncloud_grafana_${env}_uswest2_monitoring"
+    local directory="$HOME/dev/salesforce/other/personal/k8s-contexts/${env}-fkp-moncloud-api"
+    
+    cd "$directory"
+    echo "Requesting & exporting PCSKDeveloper credentials for $aws_context"
+    eval $(falcon credentials request \
+        --context "$aws_context" \
+        --role PCSKDeveloperRole \
+        --duration 744h \
+        --major-reason "Development" \
+        --details "Grafana developer, need kubectl access" \
+        | grep "export AWS_" \
+        | sed 's/INFO  Use below shell command to export AWS Credentials for account.*//')
+}
+
+_k8s_read_only_setup() {
+    local aws_context=$1
+    
+    cd ~/dev/salesforce/other/personal/tools/periscope
+    falcon context apply "$aws_context"
+    falcon kube config
+    kubectl config set-context --current --namespace moncloud-api
+}
+
+kdev1() {
+    _k8s_context_setup "dev1"
+}
+
+ktest1() {
+    _k8s_context_setup "test1"
+}
+
+kstage() {
+    _k8s_read_only_setup "moncloud_grafana_aws_esvc1_useast2_monitoring_s"
+}
+
+kprod() {
+    _k8s_read_only_setup "moncloud_grafana_aws_esvc1_useast2_monitoring"
+}
